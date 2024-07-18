0d6e3aff284f3ed111c0bb6b082132c751f27484
sora.kim <freakyk021@gmail.com>
2024-07-18 11:46:36 +0900

    2

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+// let btnSelect = document.querySelector(".btn-select");
+// let listMember = document.querySelector(".list-member");
+
+// btnSelect.addEventListener('click',function(e){
+//     // console.log("클릭했음");
+//     listMember.style.display = "block";
+// });
+// listMember.addEventListener('click',function(e){
+//     let listText = e.target.textContent;
+//     btnSelect.after(listText);
+// });
+
+
+//  수정 ------------------------------------------------------
+
 let btnSelect = document.querySelector(".btn-select");
 let listMember = document.querySelector(".list-member");
+let innerSpan = document.createElement("span");
+innerSpan.classList.add("innerSpan");
+btnSelect.after(innerSpan);
 
 btnSelect.addEventListener('click',function(e){
-    // console.log("클릭했음");
-    listMember.style.display = "block";
+    if(listMember.style.display === 'none' || listMember.style.display === ''){
+        listMember.style.display = "block";
+    }else{
+        listMember.style.display = "none";
+    }
 });
 listMember.addEventListener('click',function(e){
     let listText = e.target.textContent;
-    btnSelect.after(listText);
+    innerSpan.textContent = listText;
 });
