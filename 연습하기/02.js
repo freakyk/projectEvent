0493b754221c0f27228cd936d3e535d5be18ddb3
sora.kim <freakyk021@gmail.com>
2024-07-23 18:37:04 +0900

    XHR, Promise, Fetch 연습

---
+++ 연습하기/02.js
@@ -0,0 +1,81 @@
+// XMLHttpRequest Promise로 감싸서해보기 ------------------------------------------------------------
+
+const promise = new Promise((resolve, reject) =>{
+    const requestObj = new XMLHttpRequest();
+    requestObj.open('GET','http://test.api.weniv.co.kr/mall');
+    
+    requestObj.onreadystatechange = () => {
+        if(requestObj.readyState === 4){
+            if(requestObj.status === 200){
+                let resText = requestObj.responseText;
+                resolve(resText);
+            }else{
+                reject(new Error("에러입니다"));
+            }
+        }
+    
+    }
+    requestObj.send();
+})
+
+promise.then((reso) => {
+    // console.log(reso);
+    makeList(reso);
+}).catch((error) => {
+    console.log("error");
+});
+
+
+// 목록가져오기
+function makeList(e){
+    const $main = document.querySelector('#main');
+    const $ul = document.querySelector('ul');
+
+    const prdList = JSON.parse(e);
+    // console.log(prdList);
+    
+    for(let i=0;i<prdList.length;i++){
+        const $li = document.createElement('li');
+        const $p = document.createElement('p');
+        const $div = document.createElement('div');
+        const $img = document.createElement('img');
+        const $span = document.createElement('span');
+        const $strong = document.createElement('strong');
+        $p.append($img);
+        $div.append($span,$strong);
+        $p.classList.add('thumb');
+        $div.classList.add('detail');
+        $span.classList.add('name');
+        $strong.classList.add('price');
+        
+        $ul.append($li);
+        $li.append($p);
+        $li.append($div);
+        
+        $li.id = prdList[i].id;
+        $img.src = "http://test.api.weniv.co.kr/" + prdList[i].thumbnailImg;
+        $span.textContent = prdList[i].productName;
+        $strong.textContent = prdList[i].price;
+
+        $li.style.padding = "10px 15px 20px";
+        $li.style.margin = "5px";
+        $li.style.border = "1px solid #cccccc";
+        $li.style.listStyle = "none";
+        $li.style.width = "calc((100% - 130px) / 3)";
+        $img.style.width = "100%";
+        $img.style.border = "1px solid #e0e0e0";
+        $span.style.display = "block"
+        $span.style.fontSize = "14px"
+        $span.style.color = "gray"
+        $span.style.margin = "0 0 10px"
+        $strong.style.display = "block"
+        $strong.style.marginLeft = "auto"
+
+    }
+
+    $main.style.width = "80vw";
+    $main.style.margin = "0 auto";
+    $ul.style.display ="flex";
+    $ul.style.flexWrap ="wrap";
+
+}
---
+++ 연습하기/03.js
@@ -0,0 +1,67 @@
+// fetch로 해보기 ------------------------------------------------------------
+
+let requestObj = fetch('http://test.api.weniv.co.kr/mall');
+
+requestObj.then((req) => {
+    return req.json();
+})
+.then((resolve) => {
+    makeList(resolve);
+})
+.catch((error) => {
+    console.log("error");
+})
+
+// 목록가져오기
+function makeList(e){
+    const $main = document.querySelector('#main');
+    const $ul = document.querySelector('ul');
+
+    const prdList = e;
+    // console.log(prdList);
+    
+    for(let i=0;i<prdList.length;i++){
+        const $li = document.createElement('li');
+        const $p = document.createElement('p');
+        const $div = document.createElement('div');
+        const $img = document.createElement('img');
+        const $span = document.createElement('span');
+        const $strong = document.createElement('strong');
+        $p.append($img);
+        $div.append($span,$strong);
+        $p.classList.add('thumb');
+        $div.classList.add('detail');
+        $span.classList.add('name');
+        $strong.classList.add('price');
+        
+        $ul.append($li);
+        $li.append($p);
+        $li.append($div);
+        
+        $li.id = prdList[i].id;
+        $img.src = "http://test.api.weniv.co.kr/" + prdList[i].thumbnailImg;
+        $span.textContent = prdList[i].productName;
+        $strong.textContent = prdList[i].price;
+
+        $li.style.padding = "10px 15px 20px";
+        $li.style.margin = "5px";
+        $li.style.border = "1px solid #cccccc";
+        $li.style.listStyle = "none";
+        $li.style.width = "calc((100% - 130px) / 3)";
+        $img.style.width = "100%";
+        $img.style.border = "1px solid #e0e0e0";
+        $span.style.display = "block"
+        $span.style.fontSize = "14px"
+        $span.style.color = "gray"
+        $span.style.margin = "0 0 10px"
+        $strong.style.display = "block"
+        $strong.style.marginLeft = "auto"
+
+    }
+
+    $main.style.width = "80vw";
+    $main.style.margin = "0 auto";
+    $ul.style.display ="flex";
+    $ul.style.flexWrap ="wrap";
+
+}
